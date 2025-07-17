9a16f89fcf006e53d15694006460efffcefebe66
squeakbug <squeakbug73@outlook.com>
2025-07-17 06:38:33 +0300

    day15: part1
    Signed-off-by: squeakbug <squeakbug73@outlook.com>

--- aoc-2023/day15/main.js
+++ aoc-2023/day15/main.js
@@ -1,11 +1,74 @@
 const fs = require('node:fs');
 
+function* lazySplit(str, delimiter = ',') {
+    let start = 0;
+    let end = str.indexOf(delimiter);
+    
+    while (end !== -1) {
+        yield str.slice(start, end).trim();
+        start = end + delimiter.length;
+        end = str.indexOf(delimiter, start);
+    }
+    
+    yield str.slice(start).trim();
+}
+
+// 17 is a prime number, so ...?
+function hash(arr) {
+    return arr.split("").reduce((acc, curr) => {
+        acc += curr.charCodeAt(0);
+        acc *= 17;
+        acc %= 256;
+        return acc;
+    }, 0)
+}
+
 function part1(data) {
-    return 0
+    let totalSum = 0;
+    for (const len of lazySplit(data)) {
+        totalSum += hash(len);
+    }
+    return totalSum;
+}
+
+class Len {
+    constructor(label, focal_length) {
+        this.label = label;
+        this.focal_length = focal_length;
+    }
+}
+
+function getFocusingPower(boxes) {
+    let power = 0;
+    for (let [box_indx, box] of boxes.entries()) {
+        let box_power = 0;
+        for (let [len_indx, len] of box.entries()) {
+            box_power += (len_indx + 1) * len.focal_length;
+        }
+        box_power *= (box_indx + 1);
+        power += box_power;
+    }
+    return power;
+}
+
+function parseLenOperation(strLen) {
+    let label = "";
+    for (let ch in strLen) {
+        if (ch == '=')
+    }
+    [label, focal_length] = strLen.split("");
 }
 
 function part2(data) {
-    return 0;
+    let boxes = Array(256);
+    for (const len of lazySplit(data)) {
+        let op = parseLenOperation(len);
+        if (op == "add") {
+            // find place
+        } else {
+            // find 
+        }
+    }
 }
 
 function main() {
